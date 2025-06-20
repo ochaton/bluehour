ff3d87fb79a3204062f31e71f1dba53c14ca480a
Vladislav Grubov <vladgrubov@gmail.com>
2025-06-21 00:49:45 +0300

    fix async/await

--- src/telegram/handlers/handleInlineQuery.ts
+++ src/telegram/handlers/handleInlineQuery.ts
@@ -3,6 +3,7 @@ import { Env } from '../env';
 
 export async function handleInlineQuery(env: Env, inlineQuery: tgTypes.InlineQuery) {
 	// we expect here only to read location:
+	console.log(inlineQuery);
 	const location = inlineQuery.location;
 	if (location) {
 		await tg.sendMessage(env, {
